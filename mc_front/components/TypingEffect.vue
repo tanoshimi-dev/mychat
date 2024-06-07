fb392b2185341e8bb258a4703d2ac876935f10b8
Rehop <keijimitaki@gmail.com>
2024-06-07 21:29:05 +0900

    mychat initial commit Nuxt.js, TailwindCSS project

---
+++ mc_front/components/TypingEffect.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import {VueTypedJs} from "vue3-typed-ts";
+</script>
+
+<template>
+  <VueTypedJs
+    :strings="['Hello World', 'Hello Vue3', 'Great Experience !!!!!!🐶も行けるか']"
+    :typeSpeed="100"
+    :backSpeed="100"
+    :loop="true"
+  >
+    <span class="typing"></span>
+  </VueTypedJs>
+</template>
---
+++ mc_front/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="m-4 ">
+      <AppHeader />
+      <slot />
+      <AppFooter />
+    </div>
+  </template>
